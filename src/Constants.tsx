8f8494c9ab4abf0fbb0b23e9d17dc64b0aea7b0b
Roman Zubin <utkabulka@gmail.com>
2025-01-27 00:38:18 +0400

    add resume header

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -1,5 +1,14 @@
-export const PATH_RESUME = 'resume'
-export const PATH_CONTACTS = 'contacts'
+export const NAME = 'Nicole'
+export const LEGAL_NAME = 'Roman'
+export const SURNAME = 'Zubin'
 
+export const EMAIL = 'utkabulka@gmail.com'
+export const PHONE_NUMBER = '+995 591 06 90 84'
+export const LOCATION = 'Tbilisi, Georgia'
+export const LINK_LINKEDIN = 'linkedin.com/in/anxiouscupcake'
+export const LINK_GITHUB = 'github.com/anxiouscupcake'
+
+export const PATH_CONTACTS = 'contacts'
+export const PATH_RESUME = 'resume'
 export const PATH_PORTFOLIO = 'portfolio'
 export const PATH_BLOCKY_PARKOUR = 'blocky_parkour'
--- src/pages/resume/ResumePage.tsx
+++ src/pages/resume/ResumePage.tsx
@@ -1,7 +1,53 @@
+import '../../styles/Resume.css'
+import {
+  EMAIL,
+  LEGAL_NAME,
+  LINK_GITHUB,
+  LINK_LINKEDIN,
+  LOCATION,
+  NAME,
+  PHONE_NUMBER,
+  SURNAME,
+} from '../../Constants'
+
 function ResumePage() {
+  let useLegalName = true
+
   return (
-    <div>Resume the</div>
+    <div className='resume'>
+      <div className='header'>
+        <div className='summary-block'>
+          <h1>{getFullName(useLegalName)}</h1>
+          <h2>Software Engineer</h2>
+          <p>
+            My name is {useLegalName ? LEGAL_NAME : NAME}, I'm a Software
+            Engineer with extensive front- end development with Flutter and
+            React. I'm always striving for learning new things and very
+            passionate about technology. I believe that collaborative efforts
+            and teamwork are key to achieving great results in software
+            development.
+          </p>
+        </div>
+        <div>
+          <p>
+            <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
+          </p>
+          <p>{PHONE_NUMBER}</p>
+          <p>{LOCATION}</p>
+          <p>
+            <a href={`https://${LINK_GITHUB}`}>{LINK_GITHUB}</a>
+          </p>
+          <p>
+            <a href={`https://${LINK_LINKEDIN}`}>{LINK_LINKEDIN}</a>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default ResumePage;
+function getFullName(isLegalName: boolean): string {
+  return isLegalName ? `${LEGAL_NAME} ${SURNAME}` : `${NAME} ${SURNAME}a`
+}
+
+export default ResumePage
